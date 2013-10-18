f4247b9041ff7c6fb050a92aa6ad79f00ccd6bfd
song <song@uBox-song.(none)>
2013-10-19 03:56:07 +0800

    add login function,register function and session support

---
+++ node-mongo/public/js/login_register.js
@@ -0,0 +1,79 @@
+var page={};
+$(document).ready(function(){
+    page.bindEvent();
+});
+
+
+
+page.bindEvent=function(){
+    //lgoin page
+    $("#login").click(function(){
+        page.ajax_login({
+            username:$("#username").val(),
+            pass:$("#pass").val()
+        }); 
+    }); 
+
+    $(".login #cancel").click(function(){
+    });
+    //regeister page
+    $("#register").click(function(){
+        page.ajax_register({
+            username:$("#username").val(),
+            pass:$("#pass").val()
+        }); 
+    });
+
+    $(".register #cancel").click(function(){
+       alert("register"); 
+    });
+
+}
+
+page.ajax_login=function(json){
+    $.ajax({
+        "type":"post",
+        "url":"/ajax_login",
+        "dataType":"json",
+        "data":{"username":json.username,"pass":json.pass},
+        "success":function(result){
+            if("ok"==result.status){
+                finish_login(result);
+            }else{
+                alert(result.message);
+            }
+        },
+        "error":function(){
+        
+        }
+    });
+    function finish_login(result){
+        $("body").css({"background":"green"});
+    }
+
+};
+page.ajax_register=function(json){
+    $.ajax({
+        "type":"post",
+        "url":"/ajax_register",
+        "dataType":"json",
+        "data":{"username":json.username,"pass":json.pass},
+        "success":function(result){
+            if("ok"==result.status){
+                finish_register(result);
+            }else{
+                alert(result.message);
+            }
+        },
+        "error":function(){
+        
+        }
+    });
+
+    function finish_register(result){
+        $("body").css({"background":"green"});
+    }
+}
+
+
+
