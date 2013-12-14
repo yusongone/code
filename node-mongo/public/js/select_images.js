658dafa97df72ebff761e3c3590dd1f075a698f8
song <song@uBox-song.(none)>
2013-12-14 18:24:17 +0800

    BC

--- node-mongo/public/js/select_images.js
+++ node-mongo/public/js/select_images.js
@@ -11,6 +11,14 @@ $(document).ready(function(){
         ProductThumbList.createSelectPhoto();
     });
 });
+$(window).scroll(function(evt){
+    if($(this).scrollTop()>pTop){
+        $(".productBox").css({"position":"fixed","top":0});
+    }else{
+        $(".productBox").css({"position":"absolute","top":0});
+    };
+});
+
 var ajax_get=function(){
     $.ajax({
         "type":"post",
@@ -35,6 +43,34 @@ var ajax_get=function(){
         }
     });
 };
+
+var ajax_upload=function(data){
+    $.ajax({
+        "url":"ajax_uploadSelectPhotoList",
+        "type":"post",
+        "data":data,
+        "success":function(data){
+        }
+    });
+}
+
+var ajax_getCusProducts=function(){
+    $.ajax({
+        "type":"post",
+        "url":"/ajax_getProductsFromCustomer",
+        "dataType":"json",
+        "data":{
+            "cusInfoId":page.cusInfoId
+        },
+        "success":function(data){
+            if(data.status=="ok"&&data.data){
+            var ary=data.data;
+                ProductThumbList.init(ary);
+            }
+        }
+    });
+}
+
 
 
 var imageFactory=(function(){
@@ -74,10 +110,24 @@ var imageFactory=(function(){
 
     function image(json){
         this.body=$("<li/>",{"class":"photo"});
+        this.body.data("id",json.fileId);
         _addSlideshowData(json);
         this.id=json.fileId;
+        this.productList={};
         this.initUI(json); 
     }
+    image.prototype.addOverDiv=function(text){
+        var overSign=$("<div/>",{"class":"over "+text,"text":text})
+        var close=$("<div/>",{"class":"close fa fa-times"})
+        this.body.append(overSign.append(close));
+        //this.productList[obj.text]=overSign;
+
+        close.click(function(){
+           that.subThu(thu); 
+           overSign.remove();
+            check(thu);
+        });
+    };
     image.prototype.insertAnimate=function(){
         this.body.css({"width":"0px"}).animate({"width":"160px"},1500);
     };
@@ -95,15 +145,15 @@ var imageFactory=(function(){
         var that=this;
         json.imgBox.click(function(){
             var index=_getMeIndex.call(that);
-            console.log("myIndex",index);
             page.ss.show().to(index);
         });
-                this.body.hover(function(){
-                    var pThumb=ProductThumbList.getPThumb($(this)); 
-                    $(this).find(".nameBox").append(pThumb);
-                },function(){
-                    //z.remove();
-                });
+
+        this.body.hover(function(){
+            var pThumb=ThumbBar.getThumbTool(that); 
+            $(this).find(".nameBox").append(pThumb);
+        },function(){
+            //z.remove();
+        });
     }
     return {
         createImg:function(json){
@@ -111,7 +161,6 @@ var imageFactory=(function(){
                 imgList.push(imgObj);
                 var c=["a","b","c","d"];
                 var index=imgList.length%4;
-                    console.log(index);
                 $("."+c[index]).append(imgObj.body);
                 return imgObj;
         }
@@ -119,30 +168,59 @@ var imageFactory=(function(){
 })();
 
 
-var ajax_getCusProducts=function(){
-    $.ajax({
-        "type":"post",
-        "url":"/ajax_getProductsFromCustomer",
-        "dataType":"json",
-        "data":{
-            "cusInfoId":page.cusInfoId
-        },
-        "success":function(data){
-            if(data.status=="ok"&&data.data){
-            var ary=data.data;
-                ProductThumbList.init(ary);
-            }
-        }
-    });
-}
 
-$(window).scroll(function(evt){
-    if($(this).scrollTop()>pTop){
-        $(".productBox").css({"position":"fixed","top":0});
-    }else{
-        $(".productBox").css({"position":"absolute","top":0});
-    };
-});
+
+
+
+
+
+var ThumbBar=(function(){
+    var signList=$("<ul/>",{"class":"signList"})
+    var hoverImage;
+
+    function selectBtn(thum){
+        this.body=$("<li/>",{"class":"btnBlue sign","text":thum.text});
+        this.ThumbShow=thum;
+        signList.append(this.body);
+        this.bindEvent();
+    }
+    selectBtn.prototype.bindEvent=function(){
+        var that=this;
+        this.body.click(function(){
+            console.log($(this).data("enable"),"fefe");
+            if($(this).data("enable")){return false;};
+            that.ThumbShow.addThu(hoverImage);
+           // hoverImage.addProduct(that.ThumbShow);
+            check(hoverImage);
+        });
+    }
+
+    function check(thu){
+            signList.find(".sign").each(function(){
+                var text=thu.text;
+                console.log(thu.body.find(".A").length);
+                if(thu.body.find("."+text).length>0){
+                    $(this).addClass("disable");
+                    $(this).data("enable",1);
+                }else{
+                    $(this).data("enable",0);
+                    $(this).removeClass("disable");
+                };
+            });
+    }
+
+    return {
+       createSign:function(thum){
+           var that=this;
+           new selectBtn(thum);
+       }
+        ,getThumbTool:function(thu){
+            //check(thu);
+            hoverImage=thu;
+            return signList;
+        }
+    }
+})();
 
 var ProductThumbList=(function(){
     var charAry=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","O","R","S","T","U","V","W","X","Y","Z"];
@@ -150,20 +228,19 @@ var ProductThumbList=(function(){
     var productList=$("<ul/>",{"class":"productList"});
     var overFlowDiv=$("<div/>",{"class":"overFlowDiv"});
         overFlowDiv.append(productList);
-    var signList=$("<ul/>",{"class":"signList"})
+
     function Thum(json){
         this.id=json._id;
         this.data=json;
         this.name=this.data.name;
         this.list=[];
         this.body=$("<li/>",{"class":"productLi","title":this.data.name});
+        this.text=charAry[this.data.index];
         this.initUI();
-        this.createSign();
     } 
    Thum.prototype.initUI=function(){
-       console.log(this.data.imgCount);
        var name=$("<div/>",{"class":"textOver name","text":this.name});
-       var sign=$("<div/>",{"class":"sign","text":charAry[this.data.index]});
+       var sign=$("<div/>",{"class":"sign","text":this.text});
        this.count=$("<div/>",{"class":"count","text":0+"/"+this.data.imgCount});
        var img=$("<img/>",{"class":"headImage","src":"/public_image/"+this.data.imgPath+""})
        this.body.append(img,sign,this.count,name);
@@ -179,59 +256,25 @@ var ProductThumbList=(function(){
        }
         this.count.text(this.list.length+"/"+this.data.imgCount)
    }
-   Thum.prototype.addThu=function(thu){
+   Thum.prototype.addThu=function(imgObj){
        var that=this;
-       this.list.push(thu.data("id"));
+       this.list.push(imgObj.id);
         this.count.text(this.list.length+"/"+this.data.imgCount)
         var text=charAry[this.data.index];
-        var overSign=$("<div/>",{"class":"over "+text,"text":text})
-        var close=$("<div/>",{"class":"close fa fa-times"})
-        thu.find(".overBox").append(overSign.append(close));
-
-        close.click(function(){
-           that.subThu(thu); 
-           overSign.remove();
-            check(thu);
-        });
+        imgObj.addOverDiv(this.text);
    }
-   Thum.prototype.createSign=function(){
-       var that=this;
-       var text=charAry[this.data.index];
-        this.sign=$("<li/>",{"class":"btnBlue sign","text":text});
-        this.sign.data("text",text);
-        signList.append(this.sign);
-        this.sign.click(function(){
-            if($(this).data("enable")){return false;};
-            var thu=signList.data("thum");
-                that.addThu(thu);
-                check(thu);
-        });
-   }
-   Thum.prototype.bindEvent=function(){
-   
-   }
-
+    
    function _createThumbList(ary){
         for(var i=0;i<ary.length;i++){
             var json=ary[i];
                 json.index=i;
             var thum=new Thum(json);
             thumList.push(thum);
+            ThumbBar.createSign(thum);
         } 
    }
-    function check(thu){
-            signList.find(".sign").each(function(){
-                var text=$(this).data("text");
-                console.log(thu.find("."+text).length);
-                if(thu.find("."+text).length>0){
-                    $(this).addClass("disable");
-                    $(this).data("enable",1);
-                }else{
-                    $(this).data("enable",0);
-                    $(this).removeClass("disable");
-                };
-            });
-    }
+
+
    return {
         init:function(ary){
             var prev=$("<div/>",{"class":"prev"});
@@ -240,10 +283,6 @@ var ProductThumbList=(function(){
                 next.append("<i class='fa fa-arrow-right'></i>");
             $(".productBox").append(prev,overFlowDiv,next);
             _createThumbList(ary);
-        }
-        ,getPThumb:function(thu){
-            check(thu);
-            return signList.data("thum",thu);
         }
         ,createSelectPhoto:function(){
             var obj={};
@@ -258,24 +297,16 @@ var ProductThumbList=(function(){
         }
    }
 })();
-var ajax_upload=function(data){
-    $.ajax({
-        "url":"ajax_uploadSelectPhotoList",
-        "type":"post",
-        "data":data,
-        "success":function(data){
-            console.log(data);
-        }
-    });
-}
+
 var showSelectList=(function(){
-        var d;
+    var d;
     function initDialog(){
         d=$("<div/>",{});
             d.dialog({
                 autoOpen:false
             });
     }
+
     function createUI(data){
         for(var i in data){
             var z=$("<div/>");
